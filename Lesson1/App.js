450e07685d2e1bcd6db9d083dbbeee4631804dfb
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2025-01-19 17:10:20 +0600

    http methods, postman

--- Lesson1/App.js
+++ Lesson1/App.js
@@ -2,5 +2,20 @@ const express = require ('express');
 const app = express();
 
 
+//use methods via http
+
+app.get("/",(req,res)=>{
+    res.send("I am a get request from Home Route");
+})
+app.post("/",(req,res)=>{
+    res.send("I am a post request at Route");
+})
+app.put("/",(req,res)=>{
+    res.send("I am a put request at Route");
+})
+
+app.delete("/",(req,res)=>{
+    res.send("I am a delete request at Route");
+})
 
 module.exports = app;
